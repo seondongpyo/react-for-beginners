22bb9b5f6fd7221b8d93ddbdec79439fe15f7873
seondongpyo <seondongpyo@gmail.com>
2021-12-07 21:52:17 +0900

    refactor: create a Movie element

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -26,16 +27,14 @@ function App() {
         <div>
           {movies.map((movie) => {
             return (
-              <div key={movie.id}>
-                <img src={movie.medium_cover_image} alt={movie.title} />
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((genre) => (
-                    <li key={genre}>{genre}</li>
-                  ))}
-                </ul>
-              </div>
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
             );
           })}
         </div>
---
+++ first-react-app/src/Movie.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Movie = ({ id, coverImg, title, summary, genres }) => {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Movie;
